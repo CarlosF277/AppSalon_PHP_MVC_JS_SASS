1f9bdae11280dc8043fd8d7c901987459f5683a6
CF <cfsm.system322@gmail.com>
2022-04-22 12:32:30 -0500

    22/04/2022 12:32

---
+++ controllers/LoginController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Controllers;
+
+class LoginController {
+    public static function login(){
+        echo "Desde Login";
+    }
+
+    public static function logout(){
+        echo "Desde logout";
+    }
+
+    public static function olvide(){
+        echo "desde olvide";
+    }
+
+    public static function recuperar (){
+        echo "desde recuperar";
+    }
+
+    public static function crear (){
+        echo "desde crear";
+    }
+}
