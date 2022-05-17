b69ad71691ba3ac7b56af845b0d205629c5f565f
CF <cfsm.system322@gmail.com>
2022-05-17 17:22:28 -0500

    17/05/2022 5:22pm

--- public/index.php
+++ public/index.php
@@ -47,9 +47,9 @@ $router->post("/api/eliminar",[APIController::class,"eliminar"]);
 $router->get("/servicios", [ServicioController::class, "index"]);
 $router->get("/servicios/crear", [ServicioController::class, "crear"]);
 $router->post("/servicios/crear", [ServicioController::class, "crear"]);
-$router->get("/servicios/actualizar", [ServicioController::class], "actualizar");
-$router->post("/servicios/actualizar", [ServicioController::class], "actualizar");
-$router->get("/servicios/eliminar", [ServicioController::class], "eliminar");
+$router->get("/servicios/actualizar", [ServicioController::class, "actualizar"]);
+$router->post("/servicios/actualizar", [ServicioController::class, "actualizar"]);
+$router->post("/servicios/eliminar", [ServicioController::class, "eliminar"]);
 
 
 
